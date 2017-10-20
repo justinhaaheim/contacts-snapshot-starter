4ca1aca5f8a7499d9de071a29566b1029f5b25d2
LaToya Williams <latoyawilliams@LaToyas-MacBook-Pro.local>
2017-10-19 17:50:02 -0700

    As a user when I navigate to the /contacts/new page I should see a form which lets me create a new contact.

---
+++ test/ui/contacts.test.js
@@ -0,0 +1,24 @@
+process.env.DATABASE_URL = 'postgres://localhost:5432/contacts_test';
+const app = require('../../src/server.js');
+
+const chai = require('chai');
+const expect = require('chai').expect;
+const test = require('selenium-webdriver/testing');
+webdriver = require('selenium-webdriver');
+
+test.describe('UI Test: Use a headless browser testing library', function () {
+  test.it('I should see a form which lets me create a new contact', function () {
+    this.timeout(10000);
+
+    var driver = new webdriver.Builder()
+    .withCapabilities(webdriver.Capabilities.chrome())
+    .build();
+
+    driver.get('http://localhost:3000/contacts/new')
+
+    driver.executeScript('return document.getElementById("new-contact-form").elements.length').then((return_value) => {
+      expect(return_value).to.equal(3);
+    });
+    driver.quit();
+  })
+})
